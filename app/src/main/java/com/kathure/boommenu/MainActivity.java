69ea72f6ccf91839fb3e24f9a682eda1e44d24cc
Kathure <lizkathure@gmail.com>
2016-08-02 01:36:29 +0300

    Initial commit

---
+++ app/src/main/java/com/kathure/boommenu/MainActivity.java
@@ -0,0 +1,72 @@
+package com.kathure.boommenu;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.nightonke.boommenu.BoomMenuButton;
+import com.nightonke.boommenu.Types.BoomType;
+import com.nightonke.boommenu.Types.ButtonType;
+import com.nightonke.boommenu.Types.PlaceType;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BoomMenuButton boomMenuButton;
+    private BoomMenuButton boomMenuButtonInActionBar;
+    private BoomMenuButton boomInfo;
+    private View mCustomView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar mActionBar = getSupportActionBar();
+        mActionBar.setDisplayShowHomeEnabled(false);
+        mActionBar.setDisplayShowTitleEnabled(false);
+        LayoutInflater mInflater = LayoutInflater.from(this);
+
+        mCustomView = mInflater.inflate(R.layout.custom_actionbar, null);
+        TextView mTitleTextView = (TextView) mCustomView.findViewById(R.id.title_text);
+        mTitleTextView.setText(R.string.app_name);
+
+        boomMenuButtonInActionBar = (BoomMenuButton) mCustomView.findViewById(R.id.boom);
+        boomMenuButtonInActionBar.setOnSubButtonClickListener(this);
+        boomMenuButtonInActionBar.setAnimatorListener(this);
+
+        mActionBar.setCustomView(mCustomView);
+        mActionBar.setDisplayShowCustomEnabled(true);
+
+        ((Toolbar) mCustomView.getParent()).setContentInsetsAbsolute(0,0);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        boomMenuButton = (BoomMenuButton)findViewById(R.id.boom);
+
+    }
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        boomMenuButton.init(
+                subButtonDrawables, // The drawables of images of sub buttons. Can not be null.
+                subButtonTexts,     // The texts of sub buttons, ok to be null.
+                subButtonColors,    // The colors of sub buttons, including pressed-state and normal-state.
+                ButtonType.HAM,     // The button type.
+                BoomType.PARABOLA,  // The boom type.
+                PlaceType.HAM_3_1,  // The place type.
+                null,               // Ease type to move the sub buttons when showing.
+                null,               // Ease type to scale the sub buttons when showing.
+                null,               // Ease type to rotate the sub buttons when showing.
+                null,               // Ease type to move the sub buttons when dismissing.
+                null,               // Ease type to scale the sub buttons when dismissing.
+                null,               // Ease type to rotate the sub buttons when dismissing.
+                null                // Rotation degree.
+        );
+    }
+}
